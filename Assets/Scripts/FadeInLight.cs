693025798a436d20ae2a86adf16ea1d0a173309e
Darin Beaudreau <darinbeaudreau@gmail.com>
2016-01-29 22:26:48 -0500

    Added Play Game button and fixed lighting.

---
+++ Assets/Scripts/FadeInLight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeInLight : MonoBehaviour {
+	public float Timer;
+	public float WaitingTime;
+	public Light Lighting;
+	public Text PlayGameText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Timer += Time.deltaTime;
+		if(Timer > WaitingTime) {
+			if (Lighting.intensity < 0.8F) {
+				Lighting.intensity += 0.02F;
+				Timer = 0;
+			} else {
+				// Display the menu button text.
+				PlayGameText.text = "Play Game";
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TextHover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextHover : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.color = new Color(228, 149, 149, 255);
+	}
+
+	void OnMouseEnter() {
+		GetComponent<Renderer>().material.color = new Color(228, 66, 66, 255);
+	}
+
+	void OnMouseExit() {
+		GetComponent<Renderer>().material.color = new Color(228, 149, 149, 255);
+	}
+}
