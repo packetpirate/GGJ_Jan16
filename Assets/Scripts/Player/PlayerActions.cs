7a44e60712be58b05630627b3a4744c3810223f8
griffinplex <griffin.nozell@plexxi.com>
2016-01-29 20:48:05 -0500

    Motion
    Player game object now moves around the screen and responds to wasd

---
+++ Assets/Scripts/Player/PlayerActions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActions : MonoBehaviour {
+
+	public GameObject bomb;
+	private PlayerItems m_Items;
+
+	void Start() {
+		m_Items = GetComponent<PlayerItems> ();
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		// If action key is pressed
+		if (Input.GetButtonDown ("Action")) {
+			// If colliding with a door
+			if (other.gameObject.CompareTag ("Door") && m_Items.getKeys() > 0) {
+				// If colliding with door
+				//other.gameObject.GetComponent<DoorActions>().ChangeState();
+				m_Items.useKey();
+
+			}
+		}
+	}
+
+	public void Update(){
+
+
+		if ((Input.GetButtonDown ("Submit")) &(m_Items.getBombs() > 0) ) {
+			m_Items.useBomb();
+			Instantiate(bomb, transform.position, Quaternion.identity);
+
+		}
+
+
+	}
+}
